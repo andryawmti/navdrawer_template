857a13132127b8aafaa0a4a481a3ff9a45674c27
Heri Andria Herdiana <andri@niagahoster.co.id>
2019-02-10 18:12:37 +0700

    fix login

--- app/src/main/java/com/kudubisa/app/navdrawertemplate/SignUpActivity.java
+++ app/src/main/java/com/kudubisa/app/navdrawertemplate/SignUpActivity.java
@@ -132,7 +132,7 @@ public class SignUpActivity extends AppCompatActivity {
             userSignup.put("first_name", etFirstName.getText().toString());
             userSignup.put("last_name", etLastName.getText().toString());
             userSignup.put("email", etEmail.getText().toString());
-            userSignup.put("password", etPassword);
+            userSignup.put("password", etPassword.getText().toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -147,6 +147,7 @@ public class SignUpActivity extends AppCompatActivity {
                     if (result.getBoolean("success")) {
                         Toast.makeText(context, result.getString("message"), Toast.LENGTH_LONG).show();
                         String userJsonRaw = result.getString("user");
+                        Log.d("USER :", userJsonRaw);
                         Common.setUserRaw(userJsonRaw, context);
                         Common.setEmail(etEmail.getText().toString(), context);
                         Common.setPassword(etPassword.getText().toString(), context);
